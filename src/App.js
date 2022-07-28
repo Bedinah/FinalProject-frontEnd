37449619a7586c7cc864da66b53f60e22db85ce0
bazubagir <bazubedinah@gmail.com>
2022-07-28 12:21:30 +0200

    rebasing code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Routes from "./routes";
@@ -8,30 +7,6 @@ const App = () => {
         <Router>
             <Routes></Routes>
         </Router>);
-=======
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
->>>>>>> 5a9d074 (rebasing code)
 }
 
 export default App;
